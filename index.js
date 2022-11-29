d75175a8f12cc6460b0a2a5a0c296ae06e550a7b
LandaNahuel <landanahuel@icloud.com>
2022-11-29 20:01:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+//codigo javascript//
+/* declaracion de variables */
+//let nombre = "nahuel" ; // recomendable 
+//var apellido = 7 ; // no recomendable, malas practicas en el uso de var
+
+/*let nombre = prompt ("ingrese su nombre aqui");
+let mensaje = `Hola ${nombre}`;
+alert(mensaje);*/
+/*
+let ano = parseInt(prompt("Ingrese año de nacimiento")); 
+let actualidad = parseInt(prompt("Ingrese año actual"));
+let resta = (ano - actualidad);
+let mensaje=(`Hola tu edad actual es ${resta}`); 
+alert(mensaje);
+
+let numero1 = 1;
+let numero2 = 10;
+console.log(numero1 + numero2)
+
+if (condicion1) 
+else if(condicion2) 
+else if(condicion3)
+
+let numero=5;
+if(numero < 4){
+    console.log("es menor que 4")
+}
+else if(numero < 15){
+    console.log("es menor que 15")
+}
+else if(numero > 15){
+    console.log("es mayo o igual a 15")
+}*/
+
+
+/*let edad=35;
+let valor=edad===35;
+*/
+
+/*
+let edad= parseInt(prompt("ingrese su edad"));
+
+if(edad<14){
+    alert("no puedes entrar")
+}else if(edad<18){
+    alert("puedes entrar con un mayor")
+}else{
+    alert("puedes entrar")
+}*/
+
+
