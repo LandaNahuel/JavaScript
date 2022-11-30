32469595fe55921c4282fb658c72e4ff5c37c5a2
LandaNahuel <landanahuel@icloud.com>
2022-11-29 21:30:45 -0300

    while

--- index.js
+++ index.js
@@ -49,3 +49,48 @@ if(edad<14){
 }*/
 
 
+/*
+for(let i=0 ; i < 10; i++){ //CON ESTO REPETIMOS UN 
+    console.log("OBJETO");
+}
+*/
+
+/*
+let usuario = prompt("ingrese su usuario");
+while(usuario != "andres"){
+    alert("usuario incorrecto");
+    usuario = prompt("ingrese su usuario");
+}
+*/
+
+let entrada= prompt("Ingrese el dia").toLowerCase();
+
+while(entrada != "esc" ){
+    if(entrada == "lunes"){
+        alert(`El ${entrada} tenes dia libre`);
+        entrada= prompt("Ingrese el dia");
+    }else if(entrada == "martes"){
+        alert(`El ${entrada} tenes clase de JavaScript`);
+        entrada= prompt("Ingrese el dia");
+    }else if(entrada == "miercoles"){
+        alert(`El ${entrada} tenes cena familiar`);
+        entrada= prompt("Ingrese el dia");
+    }else if(entrada == "jueves"){
+        alert(`El ${entrada} tenes gimnasio`);
+        entrada= prompt("Ingrese el dia");
+    }else if(entrada == "viernes"){
+        alert(`El ${entrada} tenes after del laburo`);
+        entrada= prompt("Ingrese el dia");
+    }else if(entrada == "sabado"){
+        alert(`El ${entrada} tenes salida con amigos`);
+        entrada= prompt("Ingrese el dia");
+    }else if(entrada == "domingo"){
+        alert(`El ${entrada} tenes Game of Trones`);
+        entrada= prompt("Ingrese el dia");
+    }else {
+        alert("Ese dia no existe")
+        entrada= prompt("Ingrese el dia");
+    }
+    
+}
+
