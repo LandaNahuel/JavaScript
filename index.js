c4523489d9c85c5a2693b9d4284eb1f4b96fe24f
LandaNahuel <landanahuel@icloud.com>
2022-12-20 10:18:12 -0300

    correcciones

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
+const activ= ["dia libre","clase de JavaScript","cena familiar","gimnasio","after del laburo","salida con amigos","Game of Trones"];
+
 function agenda(accion){
-        while(accion != "esc" ){
-            switch (accion){
-            case "consultar":
-                let dia = prompt("ingrese el dia");
-                while(accion != "esc" ){
-                    if(dia == "lunes"){
-                        alert(`El ${dia} tenes dia libre`);
-                        break;
-                    }else if(dia == "martes"){
-                        alert(`El ${dia} tenes clase de JavaScript`);
-                        break;
-                    }else if(dia == "miercoles"){
-                        alert(`El ${dia} tenes cena familiar`);
-                        break;
-                    }else if(dia == "jueves"){
-                        alert(`El ${dia} tenes gimnasio`);
-                        break;
-                    }else if(dia == "viernes"){
-                        alert(`El ${dia} tenes after del laburo`);
-                        break;
-                    }else if(dia == "sabado"){
-                        alert(`El ${dia} tenes salida con amigos`);
-                        break;
-                    }else if(dia == "domingo"){
-                        alert(`El ${dia} tenes Game of Trones`);
-                        break;
-                    }else {
-                        alert("Ese dia no existe")
-                        break;
-                    }
+    while(accion != "esc" ){
+        switch (accion){
+        case "consultar":
+            let dia = prompt("ingrese el dia").toLowerCase();
+            while(accion != "esc" ){
+                if(dia == "lunes"){
+                    alert(`El ${dia} tenes `+ activ[0]);
+                    break;
+                }else if(dia == "martes"){
+                    alert(`El ${dia} tenes `+ activ[1]);
+                    break;
+                }else if(dia == "miercoles"){
+                    alert(`El ${dia} tenes `+ activ[2]);
+                    break;
+                }else if(dia == "jueves"){
+                    alert(`El ${dia} tenes `+ activ[3]);
+                    break;
+                }else if(dia == "viernes"){
+                    alert(`El ${dia} tenes `+ activ[4]);
+                    break;
+                }else if(dia == "sabado"){
+                    alert(`El ${dia} tenes `+ activ[5]);
+                    break;
+                }else if(dia == "domingo"){
+                    alert(`El ${dia} tenes `+ activ[6]);
+                    break;
+                }else {
+                    alert("Ese dia no existe")
+                    break;
                 }
-            case "agregar":
-                class ActividadNueva{
-                    constructor(actividad, dia, turno) {
-                        this.actividad = actividad;
-                        this.dia   = dia;
-                        this.turno  = turno;
-                    }
-                    hablar(){
-                        alert("El dia "+ this.dia +" tienes "+this.actividad+" "+this.turno);
-                    }
-                }
-                const actNew = new ActividadNueva(prompt("Ingrese una actividad"), prompt("Ingrese el dia"), prompt("Indique si es POR LA MAÑANA o POR LA TARDE").toLowerCase());
-                actNew.hablar();
-            default :
-                alert("Comando invalido")             
             }
+        case "agregar":
+            class ActividadNueva{
+                constructor(actividad, dia, turno) {
+                    this.actividad = actividad;
+                    this.dia   = dia;
+                    this.turno  = turno;
+                }
+                hablar(){
+                    alert("El dia "+ this.dia +" tienes "+this.actividad+" "+this.turno);
+                }
+            }
+            const actNew = new ActividadNueva(prompt("Ingrese una actividad").toLowerCase(), prompt("Ingrese el dia").toLowerCase(), prompt("Indique si es POR LA MAÑANA o POR LA TARDE").toLowerCase());
+            actNew.hablar();
+        default :
+            alert("Comando invalido")
         }
     }
-    
+}
+
 alert(agenda(prompt("Ustes desea CONSULTAR agenda o AGREGAR actividad")));
+
 
 
